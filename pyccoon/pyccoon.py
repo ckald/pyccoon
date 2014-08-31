622638bde4f5f62c369a4212764decbddef9951a
Andrii Magalich <andrew.magalich@gmail.com>
2014-09-01 00:50:47 +0200

    Move project description to the __init__.py

--- pyccoon/pyccoon.py
+++ pyccoon/pyccoon.py
@@ -5,24 +5,6 @@
 ![Pyccoon](pyccoon.svg)
 
 "**Pyccoon**" is a side-to-side documentation generating system.
-
-It descended from [Pyccoon](https://github.com/fitzgen/pyccoon) - a Python port of\
-[Docco](http://jashkenas.github.com/docco/):\
-the original quick-and-dirty, hundred-line-long, literate-programming-style\
-documentation generator. It produces HTML that displays your comments\
-alongside your code. Comments are passed through \
-[Markdown](http://daringfireball.net/projects/markdown/syntax), while code is\
-passed through [Pygments](http://pygments.org/) for syntax highlighting. \
-[PyStache](https://github.com/defunkt/pystache) is used for page templating.
-
-**This website is the result of running Pyccoon against its source.**
-
-Pyccoon generates the documentation folder structured correspondingly to the code. To create \
-documentation `docs` for the project in `src` folder, run the following:
-
-    pyccoon -s src -d docs
-
-[Pyccoon](https://github.com/ckald/pyccoon) is released on GitHub under the MIT license.
 """
 
 import optparse
