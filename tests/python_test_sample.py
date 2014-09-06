0915c7ce620b23e69041bae4385baef8df71ea34
Andrii Magalich <andrew.magalich@gmail.com>
2014-09-06 22:42:00 +0200

    Lost test file

---
+++ tests/python_test_sample.py
@@ -0,0 +1,34 @@
+"""
+This is a Python language test document
+"""
+
+""" for Pyccoon """
+
+# It is a indent-based language with both inline and multiline comments
+
+
+def purpose():
+    """ The purpose of this test is to verify the right sections splitting """
+    and_ = """ different features related specifically to
+              Python language """
+
+    @one_of_the_brightest(features, of, Python)
+    def are_decorators(syntax):
+        # and also the convention of writing the documentation \
+        # inside classes and functions definitions
+        pass
+
+
+class a_useful_heuristic():
+
+    # for sections splitting is indentation
+    def basically():
+        # a documentation block
+
+        # belongs to the following code only
+        if its_indentation_is >= indentation_of_the_docs:
+            # this naturally gives
+            correspondence_of_the = code
+
+        and_documentation.even_if(no_scope_definining_keywords)
+        were.used()
