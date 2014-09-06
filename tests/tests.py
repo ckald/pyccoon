da5f62fb92b60d5402df231dabc92a012fd2568a
Andrii Magalich <andrew.magalich@gmail.com>
2014-09-06 02:26:52 +0200

    Update test suites

--- tests/tests.py
+++ tests/tests.py
@@ -13,21 +13,23 @@ class DummyFileTest(unittest.TestCase):
     """
 
     input = ""
-    input_name = "__test_input__.py"
-    output_name = "__test_input__.html"
 
     def setUp(self):
         """ Initialize Pycco in the same folder as `__file__`, create a temporary input file\
             and force Pycco to run only over it."""
         folder = os.path.split(__file__)[0]
+
+        self.input_name = os.path.join(folder, "__test_input__.py")
+        self.output_name = os.path.join(folder, "__test_input__.py.html")
+
         self.pyccoon = Pyccoon({
             'sourcedir':    folder,
-            'outdir':       folder
-        })
-        with open(self.input_name, "w") as temp:
+            'outdir':       folder,
+        }, process=False)
+        with open(os.path.join(folder, self.input_name), "w") as temp:
             temp.write(self.input)
 
-        self.pyccoon.sources = [self.input_name]
+        self.pyccoon.sources = {self.input_name: (self.output_name, True)}
 
     def tearDown(self):
         """ Remove created temporary files and check if they do not exist """
@@ -37,17 +39,19 @@ class DummyFileTest(unittest.TestCase):
         assert not os.path.exists(self.output_name), "Dummy output file exists after test"
 
     def check(self, output):
-        """ Check that dummy file exist at this point. This method should be overrided in \
-            inherited classes """
+        """ Check that dummy file exist at this point. This method should be overridden """
         assert os.path.exists(self.input_name), "Dummy input file does not exist before test"
         assert os.path.exists(self.output_name), "Dummy output file does not exist before test"
 
     def test(self):
         """ The test itself. Starts pycco processing and invokes the `check` method """
-        self.pyccoon.process()
+        self.pyccoon.process([self.input_name])
         with open(self.output_name, "r") as temp:
             output = temp.read()
         self.check(output)
+
+    def shortDescription(self):
+        return self.check.__doc__
 
 
 class PyDocSubstitutions(DummyFileTest):
@@ -56,9 +60,9 @@ class PyDocSubstitutions(DummyFileTest):
                # `:pre:`"""
 
     def check(self, output):
-        assert "pydoc-return" in repr(output), "Single word PyDoc expression not converted"
-        assert "pydoc-param" in repr(output), "Multiple words PyDoc expression not converted"
-        assert "<code>:pre:</code>" in repr(output), "PyDoc expression in `pre` converted"
+        self.assertIn("pydoc-return", repr(output), "Single word PyDoc expression not converted")
+        self.assertIn("pydoc-param", repr(output), "Multiple words PyDoc expression not converted")
+        self.assertIn(":pre:", repr(output), "PyDoc expression in `pre` converted")
 
 
 class TodoSubstitutions(DummyFileTest):
@@ -71,6 +75,6 @@ class TodoSubstitutions(DummyFileTest):
 
     def check(self, output):
         assert "class=todo" in repr(output), "TODO not converted"
-        assert "<code>FIXME: something</code>" in repr(output), "FIXME in `pre` converted"
-        assert "<code>FIXME: not converted</code>" in repr(output),\
+        assert "FIXME: something" in repr(output), "FIXME in `pre` converted"
+        assert "FIXME: not converted" in repr(output),\
             "FIXME in multiline `pre` converted"
